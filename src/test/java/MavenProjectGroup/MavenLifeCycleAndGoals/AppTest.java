b5b3db3663fc7bc0ab921813c025fc2b0513b9bc
Manchana <sthamanchana@gmail.com>
2024-01-05 21:33:01 -0600

    Integrating with jenkins...

--- src/test/java/MavenProjectGroup/MavenLifeCycleAndGoals/AppTest.java
+++ src/test/java/MavenProjectGroup/MavenLifeCycleAndGoals/AppTest.java
@@ -1,20 +1,55 @@
 package MavenProjectGroup.MavenLifeCycleAndGoals;
 
-import static org.junit.Assert.assertTrue;
+import static org.testng.Assert.assertFalse;
+import static org.testng.AssertJUnit.assertTrue;
 
-import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
+	WebDriver driver;
     /**
      * Rigorous Test :-)
      */
     @Test
     public void shouldAnswerWithTrue()
     {
+    	System.out.println("Asserting Tue Test");
+
         assertTrue( true );
+    }
+
+    @Test
+    public void shouldAnswerWithFalse()
+    {
+    	System.out.println("Asserting False Test");
+
+        assertFalse(true);
+    }
+
+    
+    @BeforeTest
+    public void GetStartwithBrowser()
+    {
+    	System.out.println("Initliazing the Browser");
+    	driver = new ChromeDriver();
+    	driver.manage().window().maximize();
+    	driver.get("https://www.google.com");
+    	
+    }
+    
+    @AfterTest
+    public void CloseBrowser()
+    {
+    	System.out.println("Closing Browser");
+
+    	driver.close();
     }
 }
